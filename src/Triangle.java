949d9d2eaac0e4ea415812df0c026e75930b8a77
Ogi <ognian.yorgov@gmail.com>
2023-11-07 13:52:02 +0200

    Initial commit

---
+++ src/Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle extends ColoredFigure{
+
+    Triangle(String color,int s){
+        super(color,s);
+    }
+
+    @Override
+    String getName(){
+        return "triangle";
+    }
+
+    @Override
+    double getArea(){
+        double k=Math.sqrt(3)/4;
+        return size*size*k;
+    }
+}
